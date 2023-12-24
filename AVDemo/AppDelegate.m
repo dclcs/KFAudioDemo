dfa289c026680119d4e047345562a62754d6b9a4
dcl_1016@outlook.com <dcl.outlook.1016@gmail.com>
2023-12-24 14:08:39 +0800

    add: debug window

--- AVDemo/AppDelegate.m
+++ AVDemo/AppDelegate.m
@@ -23,9 +23,9 @@
     UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:vc];
     self.window.rootViewController = navVC;
     [self.window makeKeyAndVisible];
-//    [Debugo fireWithConfiguration:^(DGConfiguration * _Nonnull configuration) {
-//        // 配置 configuration，定制你的需求
-//    }];
+    [Debugo fireWithConfiguration:^(DGConfiguration * _Nonnull configuration) {
+        // 配置 configuration，定制你的需求
+    }];
     
     [DebugTool fireWithConfiguration];
     
