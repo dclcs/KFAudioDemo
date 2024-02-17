a323478af82cd73284e094a608cfa39bad18e599
dclcs <dcl.outlook.1016@gmail.com>
2024-02-17 10:12:43 +0800

    add runloop hang detector

---
+++ AVDemo/ViewControllers/Interview/InterviewBlockMgrViewController.h
@@ -0,0 +1,16 @@
+//
+//  InterviewBlockMgrViewController.h
+//  AVDemo
+//
+//  Created by cl d on 2024/2/17.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InterviewBlockMgrViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
